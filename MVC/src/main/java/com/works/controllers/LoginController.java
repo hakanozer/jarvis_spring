af33f6491adef655cc9ae25134cbf33c5bfeb4b5
Hakan ÖZER <hakanozer02@gmail.com>
2022-12-10 12:30:54 +0300

    RequestParam - Class Param

--- MVC/src/main/java/com/works/controllers/LoginController.java
+++ MVC/src/main/java/com/works/controllers/LoginController.java
@@ -1,4 +1,27 @@
 package com.works.controllers;
 
+import com.works.entities.Admin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class LoginController {
+
+    Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @GetMapping("/")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String adminLogin(Admin admin) {
+        logger.info( admin.toString() );
+        return "login";
+    }
+
 }
---
+++ MVC/src/main/java/com/works/entities/Admin.java
@@ -0,0 +1,11 @@
+package com.works.entities;
+
+import lombok.Data;
+
+@Data
+public class Admin {
+
+    private String email;
+    private String password;
+
+}
