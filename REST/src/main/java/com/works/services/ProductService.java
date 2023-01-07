ff11c31648bb7cb3fd5aedf0c91733dbb25aa0d5
Hakan ÖZER <hakanozer02@gmail.com>
2023-01-07 13:57:55 +0300

    Cache Manager

--- REST/src/main/java/com/works/services/ProductService.java
+++ REST/src/main/java/com/works/services/ProductService.java
@@ -4,6 +4,11 @@ import com.works.entities.Product;
 import com.works.repositories.ProductRepository;
 import com.works.utils.REnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,11 +22,15 @@ import java.util.Optional;
 public class ProductService {
 
     final ProductRepository repository;
+    final CacheManager cacheManager;
 
-    public ResponseEntity list() {
+    @Cacheable("product")
+    public ResponseEntity list(int page) {
         Map<REnum, Object> hm = new LinkedHashMap<>();
         hm.put(REnum.status, true);
-        hm.put(REnum.result, repository.findAll());
+        Pageable pageable = PageRequest.of(page, 5);
+        Page<Product> productPage = repository.findAll(pageable);
+        hm.put(REnum.result, productPage );
         return new ResponseEntity(hm, HttpStatus.OK);
     }
 
@@ -32,6 +41,7 @@ public class ProductService {
             repository.save(product);
             hm.put(REnum.status, true);
             hm.put(REnum.result, product);
+            cacheManager.getCache("product").clear();
             return new ResponseEntity(hm, HttpStatus.OK );
         }catch (Exception ex) {
             hm.put(REnum.status, false);
